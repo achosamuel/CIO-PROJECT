7372db7f946da3de6167fc5b700bda156c25fe7a
ACHO SAMUEL <samuelacho080@gmail.com>
2026-04-13 00:15:18 +0100

    Fix formatting issues in backend_app.py

--- backend_app.py
+++ backend_app.py
@@ -11,6 +11,9 @@ Pipeline:
 
 Run (PowerShell):
   uvicorn backend_app:app --host 127.0.0.1 --port 8000 --reload
+""
+Run (PowerShell):
+  uvicorn backend_app:app --host 127.0.0.1 --port 8000 --reload
 """
 
 from __future__ import annotations
